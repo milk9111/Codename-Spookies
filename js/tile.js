e099dc5f945e13d6c74e8837e8ba5019cd9fbca4
Brandon Blaschke <brandonj.blaschke@gmail.com>
2018-01-13 12:26:33 -0800

    perfomance boost

--- js/tile.js
+++ js/tile.js
@@ -17,6 +17,7 @@ class Tile {
     this.type = type;
     this.game = game;
     this.player = player;
+    this.isDraw = false;
     this.image;
     this.speedX;
     this.speedY;
@@ -26,6 +27,12 @@ class Tile {
   /** Updates a tile */
   update() {
 
+    let distance = Math.getDistance (this.player.x, this.player.y, this.x, this.y);
+    if (distance < 250) {
+      this.isDraw = true;
+    } else {
+      this.isDraw = false;
+    }
   }
 
   /** Draws the Tile on the canvas
@@ -34,6 +41,7 @@ class Tile {
   draw(ctx) {
     //(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
 
+    if (this.isDraw) {
     //Select type of sprite to show
     switch (this.type) {
       case 0:
@@ -49,7 +57,29 @@ class Tile {
         ctx.drawImage(ASSET_MANAGER.getAsset("../img/sprites.png"), 32 * 25, 32 * 9, 32, 32, this.x, this.y, 32, 32);
       break;
     }
+
     Entity.prototype.draw.call(this);
+  }
   }
 
 }
+
+/**
+ * return the distance between two points.
+ *
+ * @param {number} x1		x position of first point
+ * @param {number} y1		y position of first point
+ * @param {number} x2		x position of second point
+ * @param {number} y2		y position of second point
+ * @return {number} 		distance between given points
+ */
+Math.getDistance = function( x1, y1, x2, y2 ) {
+
+	var 	xs = x2 - x1,
+		ys = y2 - y1;
+
+	xs *= xs;
+	ys *= ys;
+
+	return Math.sqrt( xs + ys );
+};
