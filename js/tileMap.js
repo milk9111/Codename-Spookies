c668fc423f0f6f17b5457c082ef0673c7407f5af
Brandon Blaschke <brandonj.blaschke@gmail.com>
2018-01-12 19:50:00 -0800

    Add sprite img

--- js/tileMap.js
+++ js/tileMap.js
@@ -4,6 +4,7 @@ class TileMap {
   constructor () {
     this.width;
     this.height;
+    this.map2D;
   }
 
   //load and create the map
@@ -14,6 +15,15 @@ class TileMap {
       //set height and width
       this.height = txt.length;
       this.width = txt[0].length;
+
+      this.map2D = create2DArray(this.width, this.height);
+
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          this.map2D[i][j] = txt[i][j];
+        }
+      }
+      console.log(this.map2D);
     });
 
   }
@@ -25,4 +35,14 @@ class TileMap {
   draw() {
 
   }
+}
+
+function create2DArray (width, height) {
+  let temp = [width];
+
+  for (let i = 0; i < width; i++) {
+    temp[i] = new Array(height);
+  }
+
+  return temp;
 }
