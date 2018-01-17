ede12fbea7ae25f1656a475fff442c18838156f6
milk9111 <connorlndbrg349@gmail.com>
2018-01-17 14:53:46 -0800

    Refactored some objects to use ES6 format.

--- js/gameengine.js
+++ js/gameengine.js
@@ -295,20 +295,14 @@ GameEngine.prototype.loop = function () {
  *
  * @author Seth Ladd
  */
-function Entity(game, x, y) {
+/*function Entity(game, x, y) {
     this.game = game;
     this.x = x;
     this.y = y;
     this.removeFromWorld = false;
 }
 
-/**
- * The Entity's update function. It is empty because every child Entity will
- * override it with their own update method which is called in the GameEngine's
- * update.
- *
- * @author Seth Ladd
- */
+
 Entity.prototype.update = function () {
 }
 
@@ -343,10 +337,10 @@ Entity.prototype.rotateAndCache = function (image, angle) {
     //offscreenCtx.strokeStyle = "red";
     //offscreenCtx.strokeRect(0,0,size,size);
     return offscreenCanvas;
-}
+}*/
 
 //es6 version of Entity.
-/*
+
 class Entity {
 
     constructor(game, x, y) {
@@ -357,43 +351,43 @@ class Entity {
     }
 
 
-update () {
+    update () {
 
-}
+    }
 
 
-draw (ctx) {
-    if (this.game.showOutlines && this.radius) {
-        this.game.ctx.beginPath();
-        this.game.ctx.strokeStyle = "red";
-        this.game.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        this.game.ctx.stroke();
-        this.game.ctx.closePath();
+    draw (ctx) {
+        if (this.game.showOutlines && this.radius) {
+            this.game.ctx.beginPath();
+            this.game.ctx.strokeStyle = "red";
+            this.game.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            this.game.ctx.stroke();
+            this.game.ctx.closePath();
+        }
+    }
+
+
+    markForRemoval  () {
+        console.log("Marking for removal");
+        this.removeFromWorld = true;
+    }
+
+
+    rotateAndCache  (image, angle) {
+        var offscreenCanvas = document.createElement('canvas');
+        var size = Math.max(image.width, image.height);
+        offscreenCanvas.width = size;
+        offscreenCanvas.height = size;
+        var offscreenCtx = offscreenCanvas.getContext('2d');
+        offscreenCtx.save();
+        offscreenCtx.translate(size / 2, size / 2);
+        offscreenCtx.rotate(angle);
+        offscreenCtx.translate(0, 0);
+        offscreenCtx.drawImage(image, -(image.width / 2), -(image.height / 2));
+        offscreenCtx.restore();
+        //offscreenCtx.strokeStyle = "red";
+        //offscreenCtx.strokeRect(0,0,size,size);
+        return offscreenCanvas;
     }
 }
 
-
-markForRemoval  () {
-    console.log("Marking for removal");
-    this.removeFromWorld = true;
-}
-
-
-rotateAndCache  (image, angle) {
-    var offscreenCanvas = document.createElement('canvas');
-    var size = Math.max(image.width, image.height);
-    offscreenCanvas.width = size;
-    offscreenCanvas.height = size;
-    var offscreenCtx = offscreenCanvas.getContext('2d');
-    offscreenCtx.save();
-    offscreenCtx.translate(size / 2, size / 2);
-    offscreenCtx.rotate(angle);
-    offscreenCtx.translate(0, 0);
-    offscreenCtx.drawImage(image, -(image.width / 2), -(image.height / 2));
-    offscreenCtx.restore();
-    //offscreenCtx.strokeStyle = "red";
-    //offscreenCtx.strokeRect(0,0,size,size);
-    return offscreenCanvas;
-}
-}
- */
