0fdeab01b8b7e285bbe8c07320d0228474ff894f
Myles Haynes <myleschaynes@gmail.com>
2018-01-18 18:53:31 -0800

    migrate assetmanager to es6, ren heartbeat.mp3, add howler.core.min.js dependency

--- js/assetmanager.js
+++ js/assetmanager.js
@@ -1,44 +1,48 @@
-function AssetManager() {
-    this.successCount = 0;
-    this.errorCount = 0;
-    this.cache = [];
-    this.downloadQueue = [];
-}
+class AssetManager {
 
-AssetManager.prototype.queueDownload = function (path) {
-    console.log("Queueing " + path);
-    this.downloadQueue.push(path);
-}
-
-AssetManager.prototype.isDone = function () {
-    return this.downloadQueue.length === this.successCount + this.errorCount;
-}
-
-AssetManager.prototype.downloadAll = function (callback) {
-    for (var i = 0; i < this.downloadQueue.length; i++) {
-        var img = new Image();
-        var that = this;
-
-        var path = this.downloadQueue[i];
-        console.log(path);
-
-        img.addEventListener("load", function () {
-            console.log("Loaded " + this.src);
-            that.successCount++;
-            if(that.isDone()) callback();
-        });
-
-        img.addEventListener("error", function () {
-            console.log("Error loading " + this.src);
-            that.errorCount++;
-            if (that.isDone()) callback();
-        });
-
-        img.src = path;
-        this.cache[path] = img;
+    constructor() {
+        this.successCount = 0;
+        this.errorCount = 0;
+        this.cache = [];
+        this.downloadQueue = [];
     }
-}
 
-AssetManager.prototype.getAsset = function (path) {
-    return this.cache[path];
+    queueDownload(path) {
+        console.log("Queueing " + path);
+        this.downloadQueue.push(path);
+    }
+
+    isDone() {
+        return this.downloadQueue.length === this.successCount + this.errorCount;
+    }
+
+    downloadAll(callback) {
+        for (let i = 0; i < this.downloadQueue.length; i++) {
+            let img = new Image();
+            let that = this;
+
+            let path = this.downloadQueue[i];
+            console.log(path);
+
+            img.addEventListener("load", function () {
+                console.log("Loaded " + this.src);
+                that.successCount++;
+                if(that.isDone()) callback();
+            });
+
+            img.addEventListener("error", function () {
+                console.log("Error loading " + this.src);
+                that.errorCount++;
+                if (that.isDone()) callback();
+            });
+
+            img.src = path;
+            this.cache[path] = img;
+        }
+    }
+
+    getAsset = function (path) {
+        return this.cache[path];
+    }
+
 }
