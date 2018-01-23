4a48ebf68abf207c85a1679839b31c4eff6c88e6
JRob368 <jameswroberts@comcast.net>
2018-01-22 23:08:25 -0800

    side attacks in plague doctor

--- js/plagueDoctor.js
+++ js/plagueDoctor.js
@@ -21,6 +21,9 @@ class PlagueDoctor extends Enemy {
         this.walkAnimationRight = new Animation(ASSET_MANAGER.getAsset("../img/EWR.png"), 0, 0, 64, 64, 0.2, 4, true, false);
         this.walkAnimationRightAgro = new Animation(ASSET_MANAGER.getAsset("../img/EWRA.png"), 0, 0, 64, 64, 0.2, 4, true, false);
         this.attackAnimationDown = new Animation(ASSET_MANAGER.getAsset("../img/EAD.png"), 0, 0, 64, 64, 0.2, 4, true, false);
+        this.attackAnimationUp = new Animation(ASSET_MANAGER.getAsset("../img/EAU.png"), 0, 0, 64, 64, 0.2, 1, true, false);
+        this.attackAnimationLeft = new Animation(ASSET_MANAGER.getAsset("../img/EAL.png"), 0, 0, 64, 64, 0.2, 4, true, false);
+        this.attackAnimationRight = new Animation(ASSET_MANAGER.getAsset("../img/EAR.png"), 0, 0, 64, 64, 0.2, 4, true, false);
         this.notifySound = ASSET_MANAGER.getAsset("../snd/whispers.wav");
         this.notifySoundId = null;
 
@@ -34,7 +37,6 @@ class PlagueDoctor extends Enemy {
 
       let lastX = this.x;
       let lastY = this.y;
-      this.attacking = false;
 
       //Check if aggroed on the player.
       if (this.isPlayerInRange()) {
@@ -44,20 +46,30 @@ class PlagueDoctor extends Enemy {
           }
           // not close enough to attack.
           if(Math.getDistance(this.player.x, this.player.y, this.x, this.y) > 100) {
+              this.standingStill = false;
+              this.attacking = false;
               let xDir = this.player.x - this.x;
               let yDir = this.player.y - this.y;
-              if (Math.abs(xDir) > Math.abs(yDir)) {
+              //Here we need to multiply the speed by the clock like in example
+              if (Math.abs(xDir) > 20) {
                   this.unroundedX += (xDir < 0) ? -this.speed : this.speed;
                   this.x = this.unroundedX;
-              } else {
+              } else if (Math.abs(yDir) > 20){
                   this.unroundedY += (yDir) ? (yDir < 0) ? -this.speed : this.speed : 0;
                   this.y = this.unroundedY;
               }
           } else { //stand still and attack.
+              this.standingStill = true;
               this.attacking = true;
+              //If not currently attacking readjust to face player.
+              if(this.attackAnimationDown.isDone() && this.attackAnimationLeft.isDone() && this.attackAnimationRight.isDone()
+                    && this.attackAnimationUp.isDone()) {
+                  //this is never true.
+              }
           }
       } else {
-          //console.log("in here");
+          this.standingStill = true;
+          this.attacking = false;
           if (this.notifySoundId !== null && this.notifySound.playing(this.notifySoundId)) {
             this.notifySound.fade(this.notifySound.volume(), 0.0, 2000);
             this.notifySoundId = null;
@@ -101,13 +113,13 @@ class PlagueDoctor extends Enemy {
                 this.attackAnimationDown.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
                 break;
             case "up":
-                this.attackAnimationDown.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
+                this.attackAnimationUp.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
                 break;
             case "left":
-                this.attackAnimationDown.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
+                this.attackAnimationLeft.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
                 break;
             case "right":
-                this.attackAnimationDown.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
+                this.attackAnimationRight.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
                 break;
         }
 
@@ -145,7 +157,6 @@ class PlagueDoctor extends Enemy {
         switch(this.facingDirection) {
             case "down":
                 if(this.isPlayerInRange()) {
-                    console.log("in here");
                     this.walkAnimationDownAgro.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
                 } else {
                     this.walkAnimationDown.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
