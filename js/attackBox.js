b04a81edc543d5e5619524c851a122633d343151
Myles Haynes <myleschaynes@gmail.com>
2018-02-14 19:09:35 -0800

    attackbox now hits player

--- js/attackBox.js
+++ js/attackBox.js
@@ -1,21 +1,33 @@
 class AttackBox extends Entity{
     //this.swingBox = {width: 35, height: 35, x:  0, y:  0};
-    constructor(gameEngine, player, width, height, x, y, damage, facingDirection) {
+    constructor(gameEngine, player, width, height, x, y, damage, attackDirection) {
         super(gameEngine, x, y, true, width, height, 0, 0, "Attack Box");
         this.player = player;
         this.damage = damage;
         this.smackDistance = 15;
         this.smackSpeed = 3;
         this.count = 0;
-        this.facingDirection = facingDirection;
+        this.attackDirection = attackDirection;
+        this.buildDirections();
     };
+
+    buildDirections() {
+        let blocked = {};
+        blocked['right'] = 'left';
+        blocked['left'] = 'right';
+        blocked['up'] = 'down';
+        blocked['down'] = 'up';
+        this.blocked = blocked;
+    }
 
     update() {
         if(this.count >= 1) {
             this.removal = true;
         } else {
             if(Math.intersects(this, this.player)) {
-                this.player.smack(this.damage, this.smackDistance, this.facingDirection, this.smackSpeed);
+                if(this.blocked[this.attackDirection] !== facingDirection || !this.player.raising) {
+                    this.player.smack(this.damage, this.smackDistance, this.attackDirection, this.smackSpeed);
+                }
             }
         }
         this.count++;
