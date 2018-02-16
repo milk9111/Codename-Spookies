2074ad682c308541b25eeeb2124074f995b50870
Brandon Blaschke <brandonj.blaschke@gmail.com>
2018-02-16 13:44:18 -0800

    Added win menu

--- js/winScreen.js
+++ js/winScreen.js
@@ -2,35 +2,34 @@
 class WinScreen extends UIElement {
 
     constructor(game, x, y, width, height, logo) {
-        super(game, x, y, false, 100, 100, width, height, "WinScreen");
+        super(game, x, y, false, 100, 100, width, height, "TitleScreen");
 
         this.width = width;
         this.height = height;
 
         let that = this;
-        this.restartButton;
-        this.restartButton = new ImageButton(game, 0, 0,
+        this.startButton;
+        this.startButton = new ImageButton(game, 0, 0,
             150, 50, "../img/restart.png", "../img/restart_hover.png");
 
-        this.restartButton.x = UIElement.getCenterX(this.width, this.restartButton.width, this.x);
-        this.restartButton.y = UIElement.getQuarterY(this.height, this.restartButton.height, this.y) * 3;
+        this.startButton.x = UIElement.getCenterX(this.width, this.startButton.width, this.x) - 10;
+        this.startButton.y = UIElement.getQuarterY(this.height, this.startButton.height, this.y) * 3 + 160;
 
-        this.restartButton.setText = "Restart Game";
-        this.restartButton.setDefaultColor = "#333333";
-        this.restartButton.setTextFont = "15px Metal Mania";
-        let xoffset = this.restartButton.width / 2;
-        let yoffset = this.restartButton.height / 1.7;
+        this.startButton.setText = "Start Game";
+        this.startButton.setDefaultColor = "#333333";
+        this.startButton.setTextFont = "15px Metal Mania";
+        let xoffset = this.startButton.width / 2;
+        let yoffset = this.startButton.height / 1.7;
         let offsets = {
             xOffset: xoffset,
             yOffset: yoffset
         };
-        this.restartButton.setTextXandYOffset = offsets;
-        this.restartButton.setOnClick = function () {
-            console.log("I'm clicked");
+        this.startButton.setTextXandYOffset = offsets;
+        this.startButton.setOnClick = function () {
             that.game.newLevel(1);
-            that.restartButton.removal = true;
+            that.startButton.removal = true;
         };
-        game.addEntity(this.restartButton);
+        game.addEntity(this.startButton);
 
         this.game = game;
         this.logo = logo;
